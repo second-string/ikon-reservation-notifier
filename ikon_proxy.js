c410ef7c7203a43e3e4040b02b0b24eb351dbdd2
dot4qu <dot4qu@virginia.edu>
2021-01-20 15:56:16 -0800

    Actually handle auth creds refresh now

--- ikon_proxy.js
+++ ikon_proxy.js
@@ -73,6 +73,7 @@ async function ikon_login(token, cookie_str) {
         cookieJar: cookie_jar,
         ignoreInvalidCookies: true
     };
+    
 
     let res;
     let error;
@@ -118,13 +119,12 @@ async function get_ikon_reservation_dates(resort_id) {
     let res;
     let data;
     let error;
+    let error_message;
     try {
         res = await token_instance(`https://account.ikonpass.com/api/v2/reservation-availability/${resort_id}`, opts);
         data = res.body.data;
         error = !(res.statusCode >= 200 && res.statusCode <= 299);
-        console.log(`response of ${res.statusCode} but did not throw`);
     } catch (err) {
-        console.log(`Threw requesting and falling through catch block`);
         error = true;
         error_message = err.message;
     }
@@ -132,12 +132,17 @@ async function get_ikon_reservation_dates(resort_id) {
     // We've most likely be deauthed, reload token/cookies and try again
     if (res == undefined || res.statusCode >= 400) {
         console.log("Attemtping to reload token/cookies in middle of notification saving due to expiration...");
-        let { reload_error, reload_error_message, reload_data } = await load_token_and_cookies();
+        let { error: reload_error, error_message: reload_error_message, data: reload_data } = await load_token_and_cookies();
         if (reload_error) {
-            console.log(`got reload error calling load_token_and_cookies in retry block`);
             error = true;
-            error_message = "Tried to reload token/cookies after 401 but that call failed:\n";
+            error_message += "-------------------\n";
+            error_message += "Tried to reload token/cookies after 401 but that call failed:\n";
             error_message += reload_error_message;
+            return {
+                error,
+                error_message,
+                data: null
+            };
         }
 
         // Test our logged-in cookies to make sure we have acces to the api now
@@ -145,13 +150,13 @@ async function get_ikon_reservation_dates(resort_id) {
         if (!success)
         {
             error = true;
+            error_message += "--------------------\n";
             error_message = "Tried to reload token/cookies after 401. First reload call worked, but test call to /me failed";
+        } else {
+            ({ error, error_message, data } = await get_ikon_reservation_dates(resort_id));
         }
-    } else {
-        console.log(`error is ${error} and msg is ${error_message} but did not enter retry block`);
     }
 
-    console.log(`returning error ${error} and msg ${error_message}`);
     return {
         error,
         error_message: error ? error_message : null,
